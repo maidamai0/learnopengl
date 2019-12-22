bcfe05f3b32eb0551713e1c12e3486138cb180ae
tonghao.yuan <yuantonghao@gmail.com>
2019-12-22 17:16:02 +0800

    fix:fix texture generate Mipmap error

--- common/texture.h
+++ common/texture.h
@@ -11,24 +11,26 @@
  *
  */
 
+#include <limits>
+#include <string>
+
 #include "common/image_reader.h"
 #include "fmt/core.h"
 #include "glad/glad.h"
-
-#include <limits>
-#include <string>
 
 class Texture {
    public:
     explicit Texture(std::string&& image_path) {
         glGenTextures(1, &texture_);
         glBindTexture(GL_TEXTURE_2D, texture_);
+        check_error();
 
         // texture parameter
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        check_error();
 
         // read image to memory
         ImageReader image_reader{std::move(image_path)};
@@ -36,7 +38,7 @@ class Texture {
         // copy image data to OpenGL
         glTexImage2D(GL_TEXTURE_2D,
                      0,
-                     GL_RGB,
+                     image_reader.GetImageType(),
                      image_reader.GetWidth(),
                      image_reader.GetHeight(),
                      0,
@@ -44,12 +46,23 @@ class Texture {
                      GL_UNSIGNED_BYTE,
                      image_reader.GetRawData());
 
+        check_error();
+
         // generate minimap
-        glGenerateMipmap(texture_);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        check_error();
     }
 
     auto GetTextureID() const {
         return texture_;
+    }
+
+    void check_error() {
+        err_ = glGetError();
+        if (err_ != GL_NO_ERROR) {
+            fmt::print("OpenGL operation error: {}\n", err_);
+            abort();
+        }
     }
 
     ~Texture() {
@@ -58,4 +71,5 @@ class Texture {
 
    private:
     GLuint texture_{std::numeric_limits<GLuint>::max()};  // invalid value
+    GLenum err_{GL_NO_ERROR};
 };
